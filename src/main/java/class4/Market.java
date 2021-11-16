8956700817b4c7967d56af9ed50bc59b300edeeb
kpetkowska <kingapetkowska@gmail.com>
2021-11-16 11:49:57 +0100

    added final to market objects

--- src/main/java/class4/Market.java
+++ src/main/java/class4/Market.java
@@ -3,8 +3,8 @@ package class4;
 import java.util.List;
 
 public class Market {
-    private String name;
-    private List<Country> countries;
+    private final String name;
+    private final List<Country> countries;
 
     public Market(String name, List<Country> countries) {
         this.name = name;
